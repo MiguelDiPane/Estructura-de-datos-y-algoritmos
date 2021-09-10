9323ba72b1c8297835e7569ac2bbdbc278ee3cc2
MiguelDiPane <82734749+MiguelDiPane@users.noreply.github.com>
2021-09-10 09:19:07 -0300

    update

--- Unidad 3/ejercicio2/main.py
+++ Unidad 3/ejercicio2/main.py
@@ -6,15 +6,15 @@ def dec2bin(num):
         cociente = int(num / 2)
         resto = num % 2
         num = cociente
-        binario1.insertar(resto)
+        binario.insertar(resto)
 
 if __name__ == "__main__":
-    binario1 = PilaEncadenada()
+    binario = PilaEncadenada()
     num = int(input("Ingrese numero: "))
     dec2bin(num)
     
-    while not binario1.vacia():
-        x = binario1.suprimir()
+    while not binario.vacia():
+        x = binario.suprimir()
         print(x,end=" ")
 
     
