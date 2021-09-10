592f6eae292a7a1daed9896b824d3d8fdd7b6651
MiguelDiPane <82734749+MiguelDiPane@users.noreply.github.com>
2021-09-10 09:45:53 -0300

    Update clasePilaSecuencial.py

--- Unidad 3/ejercicio5/clasePilaSecuencial.py
+++ Unidad 3/ejercicio5/clasePilaSecuencial.py
@@ -9,28 +9,20 @@ class PilaSecuencial:
         self.__tope = -1
     
     def vacia(self):
-        vacia = False
-        if(self.__tope == -1):
-            vacia = True
-        return vacia
+        return self.__tope == -1
 
     def llena(self):
-        llena = False
-        if(self.__tope == self.__cant-1):
-            llena = True
-        return llena 
+        return self.__tope == self.__cant-1
 
+    #Inserta por cabeza
     def insertar(self,x):
-        if(not self.llena()):
+        if not self.llena():
             self.__items.append(x)
             self.__tope = self.__tope + 1
-        else:
-            print("Pila llena")
     
     def suprimir(self):
-        if(self.vacia()):
-            return None
-        else:
+        x = None
+        if not self.vacia():
             x = self.__items.pop()
             self.__tope = self.__tope - 1
-            return x
+        return x
